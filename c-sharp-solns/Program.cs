69b7218e06e5ec8254dd1871d06dcace5e333acc
George Seeger <george@polylytics.com>
2017-12-04 09:02:29 +0000

    Day 4 in c# like a boss

---
+++ c-sharp-solns/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using C.Sharp.Solutions.Day4;
+
+namespace C.Sharp.Solutions {
+    public class AdventOfCode {
+        public static void Main(string[] args) {
+            var validator = new PassphraseValidator();
+            Console.WriteLine(validator.NumberOfValidPassphrases);
+            Console.WriteLine(validator.NumberOfReallyValidPassphrases);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+        }
+    }
+}
