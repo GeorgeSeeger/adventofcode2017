08e8463ba80ea0dc4cc8216af8a4b5391a8dd95c
George Seeger <georgeseeger@hotmail.com>
2017-12-07 11:32:13 +0000

    part 2

--- day7/tree.rb
+++ day7/tree.rb
@@ -1,4 +1,5 @@
 class Program
+  attr_accessor :parent
   attr_reader :weight, :name, :sub_tower, :sub_names
 
   def initialize str
@@ -11,16 +12,22 @@ class Program
     @sub_tower = []
     @sub_names.each do |n|
       @sub_tower.push( programs.select{|p| p.name == n}.first)
+      @sub_tower.each do |sp| sp.parent = self end
     end
   end
 
   def balance?
     return true if !@sub_tower.any?
-    @sub_tower.all?{ |p| p.weight}
+    oneOfThemWeights = @sub_tower.first.total_weight
+    @sub_tower.all?{ |p| p.total_weight == oneOfThemWeights }
   end
 
   def total_weight
-    @weight + @sub_tower.map{|p| p.total_weight}.reduce(:+)
+    @weight + (weights_of_each.reduce(&:+) || 0)
+  end
+
+  def weights_of_each
+    @sub_tower.map{|p| p.total_weight}
   end
 end
 
@@ -28,10 +35,10 @@ class ProgramTree
   attr_reader :base, :unbalanced
 
   def initialize
-    @programs = File.open('./input').each_line.map{|str| Program.new(str)}
-    @base = find_bottom programs
-    @programs.each do p.assemble_tower programs end
-    @unbalanced = find_unbalanced programs
+    @programs = File.open('./input').each_line.map{|str| Program.new(str)}.reject(&:nil?)
+    @base = find_bottom @programs
+    @programs.each do |p| p.assemble_tower @programs end
+    @unbalanced = find_unbalanced @base
   end
 
   def find_bottom arr
@@ -47,11 +54,15 @@ class ProgramTree
     bottomProgram
   end
 
-  def find_unbalanced arr
-    consider = find
-    
+  def find_unbalanced program
+    return program if program.balance?
+    weights = program.sub_tower.map(&:total_weight)
+    find_unbalanced program.sub_tower.select{|p| weights.count{|w| w == p.total_weight} == 1 }.first
   end
 end
 
 
-puts ProgramTree.new.base.name
+tree = ProgramTree.new
+puts tree.base.name
+puts "#{tree.unbalanced.name} is #{tree.unbalanced.weight} weight, total of #{tree.unbalanced.total_weight}
+  it should total #{tree.unbalanced.parent.sub_tower.select{|p| p.balance?}.first.total_weight}"
