8feb69a130e5593885634db5fbdfc27cc3e2928a
George Seeger <george@polylytics.com>
2017-12-18 13:52:17 +0000

    day18 part 2 is proving quite hard

--- day18/duet.js
+++ day18/duet.js
@@ -1,40 +1,74 @@
 class Duet {
-    constructor(input) {
+    constructor(input, id, queue) {
         this.input = input;
-        this.lastPlayed = null;
         this.pointer = 0;
-        this.recovered = null;
         this.reg = {};
-        this.initReg();
+        this.queue = queue;
+        this.id = id;
+        this.awaiting = false;
+        this.initReg(id);
         this.fcts = {
             "add": (a, v) => this.reg[a] += v,
             "mul": (a, v) => this.reg[a] *= v,
             "set": (a, v) => this.reg[a] = +v,
             "mod": (a,v ) => this.reg[a] %= v,
             "jgz": (a, v) => (+a > 0 || isNaN(a) && this.reg[a] > 0) ? this.pointer += v - 1 : v,
-            "snd": (a) => this.lastPlayed = this.reg[a],
-            "rcv": (a) => this.reg[a] > 0 ? console.log(this.recovered = this.lastPlayed) : null,
+            "snd": (a) => this.queue.enqueue(id, isNaN(a) ?  this.reg[a] : +a ),
+            "rcv": (a) => this.receiveAt(a),
         }
         this.carryOut();
     }
 
-    initReg() {
+    initReg(id) {
        this.input.map(inst => inst.split(' ')[1]).filter(v => isNaN(v)).forEach(a => this.reg[a] = 0);
+       this.reg["p"] = id;
     }
 
     carryOut() {
-        for (var i = 0;this.pointer >= 0 && i < 2000/* this.pointer < this.input.length */; this.pointer++, i++) {
+        for (;this.pointer >= 0 && this.pointer < this.input.length && !this.awaiting; this.pointer++) {
             var getVal = (val) => isNaN(val) ? (Object.keys(this.reg).includes(val) ? this.reg[val] : null) : +val;
             var inst = this.input[this.pointer].split(" ");
-            // console.log(this.input[this.pointer] + ": " + this.pointer)
             this.fcts[inst[0]](inst[1], getVal(inst[2]));
-            // console.log(this.reg);
         }
-        // console.log(this);
+    }
+
+    receiveAt(a) {
+        var id = this.id == 0 ? 1 : 0;
+        if (this.queue.queues[id].length == 0) {
+            this.awaiting = true;
+        } else {
+            this.reg[a] = this.queue.dequeue(id)
+        }
     }
 }
 
+class MessageQueue {
+    constructor() {
+        this.p1Counter=0;
+        this.queues = { 0: [], 1: []};
+    }
+
+    enqueue(id, val) {
+        if (id == 1) this.p1Counter++;
+        this.queues[id].push(val);
+    }
+
+    dequeue(id) {
+        return this.queues[id].pop();
+    }
+}
+
+
 var fs = require("fs");
-var input = fs.readFileSync("./input", "utf8").split("\n");
+var input = fs.readFileSync("./day18/input", "utf8").split("\n");
 // var input = fs.readFileSync("./test", "utf8").split("\r\n");
-var duet = new Duet(input);
+var queue = new MessageQueue();
+var p0 = new Duet(input, 0, queue);
+var p1 = new Duet(input, 1, queue);
+
+while (Object.values(queue.queues).some(a => a.length > 0)) {
+    p0.carryOut();
+    p1.carryOut();
+}
+
+console.log(queue.p1Counter);
