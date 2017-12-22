cfb09b287333812b58bddb7bd0686dd3c2483626
George Seeger <georgeseeger@hotmail.com>
2017-12-22 20:55:13 +0000

    day22 part 2: significantly less elegant than part 1

--- day22/virus.rb
+++ day22/virus.rb
@@ -1,19 +1,21 @@
 class Virus
   attr_reader :grid, :infections, :infected, :dir, :pos
-  def initialize input
-    @grid = input.map(&:chars)
-    @pos = [(input.length/2).to_i, (input.first.length / 2).to_i]
+  def initialize input, size
+    @grid = Array.new(size) {Array.new(size, ".")}
+    put_in_middle input.map(&:chars)
+    @pos = [(@grid.length/2).to_i, (@grid.first.length / 2).to_i]
     @dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
     @dir = [-1, 0]
-    @infected = @grid.map.with_index{|g, i| g.map.with_index{|v, j| v == '#' ? [i, j] : nil }.reject(&:nil?)}.flatten(1)
     @infections = 0
   end
 
   def burst
     if val == "#"
       turn_right
-    else
+    elsif val == "."
       turn_left
+    elsif val == "F"
+      turn_around
     end
     clean_or_infect
     move    
@@ -23,7 +25,6 @@ class Virus
   def move
     @pos[0] += @dir[0]
     @pos[1] += @dir[1]
-    expand
   end
 
   def val 
@@ -44,42 +45,42 @@ class Virus
     @dir = @dirs[(index + 1) % 4]
   end
 
+  def turn_around
+    @dir = @dir.map{|i| i * -1}
+  end
+
   def clean_or_infect
     if val == "."
-      set_val('#')
-      @infections += 1 #unless @infected.include?(@pos)
-      # @infected.push(@pos.dup)
+      set_val('W')
+    elsif val == "W"
+      @infections += 1
+      set_val("#")
+    elsif val == "#"
+      set_val('F')
     else
       set_val('.')
     end  
   end
 
-  def expand
-    if @pos[0] < 0
-      @pos[0] += 1
-      @grid.unshift(Array.new(@grid.first.length, "."))
-      @infected.each{|a| a[0] += 1}
-    elsif @pos[0] >= @grid.length
-      @grid.push(Array.new(@grid.first.length, "."))
+  def put_in_middle arr
+    low = @grid.length / 2 - arr.length / 2
+    low_inner = @grid.first.length / 2 - arr.first.length / 2
+    (low...low+arr.length).each do |i|
+      (low_inner...low_inner+arr.length).each do |j|
+        @grid[i][j] = arr[i - low][j - low_inner]
+      end
     end
-    if @pos[1] < 0
-      @pos[1] += 1
-      @grid.each{|a| a.unshift('.')}
-      @infected.each{|a| a[1] += 1}
-    elsif @pos[1] >= @grid.first.length
-      @grid.each{|a| a.push('.')}
-    end
+    
   end
 end
 
 test = "..#/#../...".split("/")
-virus = Virus.new( File.open("./day22/input").each_line.to_a)
-10000.times do
-   virus.burst
-  #  puts virus.grid.map{|g| g.join("")}
-  #  p [virus.infections, virus.pos, virus.dir]
-  #  sleep(0.01)
-  #  system("clear")
+virus = Virus.new(File.open("./day22/input").each_line.map(&:chomp).to_a, 1001)
+10000000.times do |i|
+  virus.burst
+  # puts virus.grid.map{|g| g.join("")}
+  # sleep(0.02)
+  # system("clear")
+  puts "#{i/100000}% done" if i %100000 == 0
 end
-#puts virus.grid.map{|g| g.join("")}
 puts virus.infections
